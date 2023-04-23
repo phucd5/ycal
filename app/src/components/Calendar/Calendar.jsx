408d613ee2be4a72d6953141e2d39302b42252df
ewang001 <eric.wang075@gmail.com>
2023-04-23 16:04:39 -0400

    friends modal

--- app/src/components/Calendar/Calendar.jsx
+++ app/src/components/Calendar/Calendar.jsx
@@ -12,6 +12,7 @@ import CreateEventForm from "../Dialog/CreateEventForm";
 import AddFriendDialog from "../Dialog/AddFriend";
 import EventDetailsDialog from "../Dialog/EventDetailsDialog";
 import AddCourseDialog from "../Dialog/AddCourseDialog";
+import FriendsDialog from "../Dialog/FriendsDialog";
 
 import MeetingDialog from "../Dialog/MeetingDialog";
 import CourseDetailsDialog from "../Dialog/CourseDetailsDialog";
@@ -24,6 +25,7 @@ const Calendar = () => {
 
 	const [eventModalShow, setEventModalShow] = useState(false);
 	const [courseModalShow, setCourseModalShow] = useState(false);
+	const [friendsModalShow, setFriendsModalShow] = useState(false);
 	const [selectedEvent, setSelectedEvent] = useState(null);
 	const [selectedCourse, setSelectedCourse] = useState(null);
 
@@ -31,6 +33,8 @@ const Calendar = () => {
 	const handleEventModalClose = () => setEventModalShow(false);
 	const handleCourseModalShow = () => setCourseModalShow(true);
 	const handleCourseModalClose = () => setCourseModalShow(false);
+	const handleFriendsModalShow = () => setFriendsModalShow(true);
+	const handleFriendsModalClose = () => setFriendsModalShow(false);
 	const handleSelectedEvent = (event) => setSelectedEvent(event);
 
 	const [user, setUser] = useState(null);
@@ -123,58 +127,58 @@ const Calendar = () => {
 
 	/* Callback Functions Requests */
 
-	const handleAddFriend = async (friendId, friendEmail) => {
-		try {
-			const response = await axios.get(
-				`http://localhost:3002/users/${friendEmail}/email`
-			);
-			try {
-				const response_2 = await axios.put(
-					`http://localhost:3002/users/${user._id}/friends`,
-					{
-						friendId: response.data._id,
-						action: "add",
-					}
-				);
-				setFriends(response_2.data);
-				handleRemoveFriendRequest(friendId);
-			} catch (error) {
-				alert("Person is already in your friend's list!");
-			}
-		} catch (error) {
-			alert("Can't find friend", error);
-		}
-	};
+	// const handleAddFriend = async (friendId, friendEmail) => {
+	// 	try {
+	// 		const response = await axios.get(
+	// 			`http://localhost:3002/users/${friendEmail}/email`
+	// 		);
+	// 		try {
+	// 			const response_2 = await axios.put(
+	// 				`http://localhost:3002/users/${user._id}/friends`,
+	// 				{
+	// 					friendId: response.data._id,
+	// 					action: "add",
+	// 				}
+	// 			);
+	// 			setFriends(response_2.data);
+	// 			handleRemoveFriendRequest(friendId);
+	// 		} catch (error) {
+	// 			alert("Person is already in your friend's list!");
+	// 		}
+	// 	} catch (error) {
+	// 		alert("Can't find friend", error);
+	// 	}
+	// };
 
-	const handleRemoveFriendRequest = async (friendId) => {
-		try {
-			const response = await axios.put(
-				`http://localhost:3002/users/${user._id}/friendrequests`,
-				{
-					friendId: friendId,
-					action: "remove",
-				}
-			);
-			setFriendRequests(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+	// const handleRemoveFriendRequest = async (friendId) => {
+	// 	try {
+	// 		const response = await axios.put(
+	// 			`http://localhost:3002/users/${user._id}/friendrequests`,
+	// 			{
+	// 				friendId: friendId,
+	// 				action: "remove",
+	// 			}
+	// 		);
+	// 		setFriendRequests(response.data);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
 
-	const handleRemoveFriend = async (friendId) => {
-		try {
-			const response = await axios.put(
-				`http://localhost:3002/users/${user._id}/friends`,
-				{
-					friendId: friendId,
-					action: "remove",
-				}
-			);
-			setFriends(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+	// const handleRemoveFriend = async (friendId) => {
+	// 	try {
+	// 		const response = await axios.put(
+	// 			`http://localhost:3002/users/${user._id}/friends`,
+	// 			{
+	// 				friendId: friendId,
+	// 				action: "remove",
+	// 			}
+	// 		);
+	// 		setFriends(response.data);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
 
 	const handleEventClick = async (info) => {
 		if (info.event.extendedProps.isClass) {
@@ -188,100 +192,100 @@ const Calendar = () => {
 
 	/* Render component functions */
 
-	const renderFriendRequests = () => {
-		return (
-			<>
-				<table style={{ marginTop: "10px" }}>
-					<thead>
-						<tr>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email</th>
-						</tr>
-					</thead>
-					<tbody>
-						{friendRequests.map((friend) => (
-							<tr key={friend._id}>
-								<td>{friend.firstName}</td>
-								<td>{friend.lastName}</td>
-								<td>{friend.email}</td>
-								<td>
-									<button
-										style={{
-											marginRight: "15px",
-											marginLeft: "15px",
-										}}
-										onClick={() =>
-											handleRemoveFriendRequest(
-												friend._id
-											)
-										}
-									>
-										Delete Request
-									</button>
-									<button
-										style={{
-											marginRight: "15px",
-											marginLeft: "15px",
-										}}
-										onClick={() =>
-											handleAddFriend(
-												friend._id,
-												friend.email
-											)
-										}
-									>
-										Add Friend
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</>
-		);
-	};
+	// const renderFriendRequests = () => {
+	// 	return (
+	// 		<>
+	// 			<table style={{ marginTop: "10px" }}>
+	// 				<thead>
+	// 					<tr>
+	// 						<th>First Name</th>
+	// 						<th>Last Name</th>
+	// 						<th>Email</th>
+	// 					</tr>
+	// 				</thead>
+	// 				<tbody>
+	// 					{friendRequests.map((friend) => (
+	// 						<tr key={friend._id}>
+	// 							<td>{friend.firstName}</td>
+	// 							<td>{friend.lastName}</td>
+	// 							<td>{friend.email}</td>
+	// 							<td>
+	// 								<button
+	// 									style={{
+	// 										marginRight: "15px",
+	// 										marginLeft: "15px",
+	// 									}}
+	// 									onClick={() =>
+	// 										handleRemoveFriendRequest(
+	// 											friend._id
+	// 										)
+	// 									}
+	// 								>
+	// 									Delete Request
+	// 								</button>
+	// 								<button
+	// 									style={{
+	// 										marginRight: "15px",
+	// 										marginLeft: "15px",
+	// 									}}
+	// 									onClick={() =>
+	// 										handleAddFriend(
+	// 											friend._id,
+	// 											friend.email
+	// 										)
+	// 									}
+	// 								>
+	// 									Add Friend
+	// 								</button>
+	// 							</td>
+	// 						</tr>
+	// 					))}
+	// 				</tbody>
+	// 			</table>
+	// 		</>
+	// 	);
+	// };
 
-	const renderFriendsLists = () => {
-		return (
-			<>
-				<table style={{ marginTop: "10px" }}>
-					<thead>
-						<tr>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email</th>
-						</tr>
-					</thead>
-					<tbody>
-						{friends.map((friend) => (
-							<tr key={friend._id}>
-								<td>{friend.firstName}</td>
-								<td>{friend.lastName}</td>
-								<td>{friend.email}</td>
-								<td>
-									<button
-										style={{
-											marginRight: "15px",
-											marginLeft: "15px",
-										}}
-										onClick={() =>
-											handleRemoveFriend(friend._id)
-										}
-									>
-										Delete Friend
-									</button>
-									<FriendsCalandar
-										friendId={friend._id}
-									></FriendsCalandar>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</>
-		);
-	};
+	// const renderFriendsLists = () => {
+	// 	return (
+	// 		<>
+	// 			<table style={{ marginTop: "10px" }}>
+	// 				<thead>
+	// 					<tr>
+	// 						<th>First Name</th>
+	// 						<th>Last Name</th>
+	// 						<th>Email</th>
+	// 					</tr>
+	// 				</thead>
+	// 				<tbody>
+	// 					{friends.map((friend) => (
+	// 						<tr key={friend._id}>
+	// 							<td>{friend.firstName}</td>
+	// 							<td>{friend.lastName}</td>
+	// 							<td>{friend.email}</td>
+	// 							<td>
+	// 								<button
+	// 									style={{
+	// 										marginRight: "15px",
+	// 										marginLeft: "15px",
+	// 									}}
+	// 									onClick={() =>
+	// 										handleRemoveFriend(friend._id)
+	// 									}
+	// 								>
+	// 									Delete Friend
+	// 								</button>
+	// 								<FriendsCalandar
+	// 									friendId={friend._id}
+	// 								></FriendsCalandar>
+	// 							</td>
+	// 						</tr>
+	// 					))}
+	// 				</tbody>
+	// 			</table>
+	// 		</>
+	// 	);
+	// };
 
 	const renderDialogs = () => {
 		return (
@@ -311,18 +315,28 @@ const Calendar = () => {
 				<strong>YCal</strong>
 			</h1>
 			<div className={styled.bootstrap}>
-				<AddFriendDialog user={user} />
+				{/* <AddFriendDialog user={user} /> */}
+				<FriendsDialog 
+					user={user}
+					friends={friends}
+					friendRequests={friendRequests}
+					setFriends={setFriends}
+					setFriendRequests={setFriendRequests}
+					show={friendsModalShow}
+					handleShow={handleFriendsModalShow}
+					handleClose={handleFriendsModalClose}
+				/>
 				<div class="container">
-					<div class="item-friends-item">
+					{/* <div class="item-friends-item">
 						{renderFriendRequests()}
 						{renderFriendsLists()}
-					</div>
+					</div> */}
 					<div class="item-courses-item">
 						<h2>Courses:</h2>
 						<AddCourseDialog
 							user={user}
 							fetchClasses={fetchCourses}
-						/>
+					/>
 					</div>
 					<h2>My Calendar</h2>
 					<div class="item-events-item">
