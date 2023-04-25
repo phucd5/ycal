b8bd9ecd04cd976f445657e3df5e9bef767df08b
phucd5 <phucduongh@gmail.com>
2023-04-25 17:19:54 -0400

    use cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+	cors({
+		origin: "*", // set to * to allow any origin to access the server's resources
+	})
+);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
