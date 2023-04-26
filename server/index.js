aae74ff94cf93ffbb3da78c2ca5c4f5859f583fe
phucd5 <phucduongh@gmail.com>
2023-04-25 21:40:17 -0400

    Course

--- server/index.js
+++ server/index.js
@@ -25,10 +25,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
 	cors({
-		origin: ["*"],
+		origin: "*",
 	})
 );
-
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
